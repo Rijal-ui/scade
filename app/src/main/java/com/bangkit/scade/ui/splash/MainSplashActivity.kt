82ce08d1b30a048f7b2ce23564a474bbe5f31e16
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-05-14 07:27:55 +0700

    Initial commit

---
+++ app/src/main/java/com/bangkit/scade/ui/splash/MainSplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.scade.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.scade.R
+
+class MainSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_splash)
+    }
+}
