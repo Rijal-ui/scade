c34588abf785eefcb704d6034b41df06b1a7bae3
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-05-16 18:37:19 +0700

    Add Bottom Navigation

--- app/src/main/java/com/bangkit/scade/ui/splash/MainSplashActivity.kt
+++ app/src/main/java/com/bangkit/scade/ui/splash/MainSplashActivity.kt
@@ -1,12 +1,27 @@
 package com.bangkit.scade.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.bangkit.scade.R
+import com.bangkit.scade.ui.home.HomeActivity
 
 class MainSplashActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_splash)
+
+        handler = Handler(mainLooper)
+        handler.postDelayed(
+            {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000
+        )
+
     }
 }
