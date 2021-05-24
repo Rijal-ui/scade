b730419f5371eb37020ad9e2fe959e6c7d69a4d6
Dimas Yuda Wahyu Buana <dimas.bjg@gmail.com>
2021-05-24 19:58:25 +0700

    Add register and solving error handling

--- app/src/main/java/com/bangkit/scade/data/source/remote/response/RegisterResponse.kt
+++ app/src/main/java/com/bangkit/scade/data/source/remote/response/RegisterResponse.kt
@@ -2,7 +2,7 @@ package com.bangkit.scade.data.source.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class SignUpResponse(
+data class RegisterResponse(
 
 	@field:SerializedName("data")
 	val data: Any? = null,
